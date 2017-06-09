04a469e14280faf9a31db36b4c9d3c90063d53f9
sebastienserre <sebastien@thivinfo.com>
2017-06-09 17:58:46 +0200

    ACF PRO loaded

--- wp-pharma.php
+++ wp-pharma.php
@@ -14,7 +14,8 @@ Domain Path: /languages
 define( 'WP_PHARMA_VERSION', '0.1.0' );
 define( 'WP_PHARMA_FOLDER', 'wp-pharma' );
 define( 'WP_PHARMA_URL', plugin_dir_url( __FILE__ ) );
-define( 'WP_PHARMA_DIR', plugin_dir_path( __FILE__ ) );
+define( 'WP_PHARMA_PATH', plugin_dir_path( __FILE__ ) );
+var_dump(WP_PHARMA_URL);
 
 // Load Languages
 add_action( 'plugins_loaded', 'wp_pharma_load_textdomain');
@@ -22,3 +23,44 @@ function wp_pharma_load_textdomain() {
 	load_plugin_textdomain( 'wp-pharma', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 }
 
+/**
+ * Load ACF PRO
+ */
+
+add_filter('acf/settings/path', 'wp_pharma_acf_settings_path');
+
+function wp_pharma_acf_settings_path( $path ) {
+
+	// update path
+	$path = WP_PHARMA_PATH . '/inc/acf/';
+
+	// return
+	return $path;
+
+}
+
+
+// 2. customize ACF dir
+add_filter('acf/settings/dir', 'wp_pharma_acf_settings_dir');
+
+function wp_pharma_acf_settings_dir( $dir ) {
+
+	// update path
+	$dir = WP_PHARMA_URL . 'inc/acf/';
+
+	// return
+	return $dir;
+
+}
+
+
+// 3. Hide ACF field group menu item
+add_filter('acf/settings/show_admin', '__return_true');
+
+
+// 4. Include ACF
+
+add_action('plugins_loaded', 'wp_pharma_load_acf');
+function wp_pharma_load_acf() {
+	include_once( WP_PHARMA_PATH . 'inc/acf/acf.php' );
+}
