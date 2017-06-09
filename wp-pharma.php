3deb4015671cb2fd259a430b54e7671f9c8ac926
sebastienserre <sebastien@thivinfo.com>
2017-06-09 17:28:33 +0200

    define basic constant

--- wp-pharma.php
+++ wp-pharma.php
@@ -10,8 +10,15 @@ Text Domain: wp-pharma
 Domain Path: /languages
  */
 
+// Plugin constants
+define( 'WP_PHARMA_VERSION', '0.1.0' );
+define( 'WP_PHARMA_FOLDER', 'wp-pharma' );
+define( 'WP_PHARMA_URL', plugin_dir_url( __FILE__ ) );
+define( 'WP_PHARMA_DIR', plugin_dir_path( __FILE__ ) );
 
+// Load Languages
 add_action( 'plugins_loaded', 'wp_pharma_load_textdomain');
 function wp_pharma_load_textdomain() {
 	load_plugin_textdomain( 'wp-pharma', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 }
+
