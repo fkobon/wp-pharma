74422ef259a99fb43b0d1bb733ce10b32f224c7c
sebastienserre <sebastien@thivinfo.com>
2017-06-23 06:36:08 +0200

    correct patient role

--- inc/role.php
+++ inc/role.php
@@ -19,26 +19,26 @@ add_action('admin_init', 'wp_pharma_add_patient_role');
 function wp_pharma_add_patient_role(){
 
 	// uncomment to remove role
-/*	if( get_role('patient') ){
-		remove_role( 'patient' );
-	}
-*/
-	if ( ! get_role( 'patient' ) ) {
-		add_role( 'patient', 'Patient', array( 'read' => true, 'level_0' => true ) );
-	}
+	/*	if( get_role('patient') ){
+			remove_role( 'patient' );
+	*/
+		if ( ! get_role( 'patient' ) ) {
+			add_role( 'patient', 'Patient', array( 'read' => true, 'level_0' => true ) );
+		}
 
-	$roles = get_editable_roles();
-	$role = get_role( 'patient' );
-	if ( $roles['patient'] ) {
-		$role->add_cap( 'edit_ordo' );
-		$role->add_cap( 'edit_ordos' );
-		$role->add_cap( 'read_ordo' );
-		$role->add_cap( 'read' );
-		$role->add_cap( 'create_ordos' );
-		$role->add_cap( 'edit_published_ordos' );
-		$role->add_cap( 'publish_ordos' );
-		
-	}
+		$roles = get_editable_roles();
+		$role = get_role( 'patient' );
+		if ( $roles['patient'] ) {
+			$role->add_cap( 'edit_ordo' );
+			$role->add_cap( 'edit_ordos' );
+			$role->add_cap( 'read_ordo' );
+			$role->add_cap( 'read' );
+			$role->add_cap( 'create_ordos' );
+			$role->add_cap( 'edit_published_ordos' );
+			$role->add_cap( 'publish_ordos' );
+
+		}
+
 
 	$role = get_role( 'administrator' );
 	if ( $role ) {
